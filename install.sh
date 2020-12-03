7090583eba8d79f93dd418b23bce8332c9778093
Thani324 <thani@leewayhertz.com>
2020-12-03 22:01:13 +0530

    fixes

--- install.sh
+++ install.sh
@@ -12,10 +12,10 @@ apt-get install -y $STELLAR_CORE_BUILD_DEPS
 
 # clone, compile, and install stellar core
 #git clone --branch $STELLAR_CORE_VERSION --recursive --depth 1 https://github.com/thanigaivel97/sellar-core.git -b stellar-14.1
-git clone https://github.com/thanigaivel97/stellar-core-prod.git -b inflation
+git clone https://github.com/thanigaivel97/stellar-core.git -b inflation
 
 
-cd stellar-core-prod
+cd stellar-core
 chmod 777 *
 git submodule update --init --recursive
 bash autogen.sh
@@ -23,12 +23,6 @@ bash autogen.sh
 make
 make install
 cd ..
-
-
-# install confd for config file management
-wget -nv -O /usr/local/bin/confd https://github.com/kelseyhightower/confd/releases/download/v${​​​​​​​CONFD_VERSION}​​​​​​​/confd-${​​​​​​​CONFD_VERSION}​​​​​​​-linux-amd64
-chmod +x /usr/local/bin/confd
-
 
 # cleanup
 rm -rf stellar-core
